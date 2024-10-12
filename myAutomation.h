d2fed66173a01f7b1173a716174f3d33a983c382
Martin <martin-vl@users.noreply.github.com>
2024-10-12 21:59:27 +0200

    Added second ioexpander

---
+++ myAutomation.h
@@ -0,0 +1,16 @@
+#define PULSE 20    // Set the duration of the pulse to 10ms
+
+#define DUAL_COIL_TURNOUT(id, en, in1, in2, desc) \
+VIRTUAL_TURNOUT(id, desc) \
+DONE \
+ONCLOSE(id) \
+RESET(in2) SET(in1) \
+SET(en) DELAY(PULSE) RESET(en) \
+DONE \
+ONTHROW(id) \
+RESET(in1) SET(in2) \
+SET(en) DELAY(PULSE) RESET(en) \
+DONE
+
+DUAL_COIL_TURNOUT(1, 168, 164, 165, "Wissel A")
+DUAL_COIL_TURNOUT(2, 169, 166, 167, "Wissel B")
---
+++ mySetup.h
@@ -0,0 +1,8 @@
+// set enable pins of turnout low/off
+IODevice::write(168, 0);
+IODevice::write(169, 0);
+
+// Set DC for track A, loc 1
+SETUP("<= A DC 1>");
+
+Sensor::create(5000, 5000, 0);
