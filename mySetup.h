b585df9b010f54735f1d82571c065e47cc3b4028
Martin <martin-vl@users.noreply.github.com>
2024-10-18 21:29:18 +0200

    Update mySetup.h

--- mySetup.h
+++ mySetup.h
@@ -4,7 +4,9 @@ IODevice::write(169, 0);
 
 // Set DC for track A, loc 1
 SETUP("<= A DC 1>");
+// Create input buttons
 SETUP("<S 20 170 pullup>");
+// Create output LEDs for buttons
 SETUP("<Z 21 171 100>");
 
 Sensor::create(5000, 5000, 0);
