e56dc187cff05605d8b538fb3b8f60569764d042
Martin <martin-vl@users.noreply.github.com>
2024-10-19 05:59:32 +0200

    Update myAutomation.h

--- myAutomation.h
+++ myAutomation.h
@@ -4,9 +4,13 @@ ALIAS(BT1_SW, 170)
 ALIAS(BT1_LED, 171)
 //SIGNALH(BT1_LED, 0, 0)
 
+// Define Routes
+ALIAS(RT1)
+ALIAS(RT2)
+
 
 // Define Turnouts
-#define PULSE 200    // Set the duration of the pulse to 10ms
+#define PULSE 10000    // Set the duration of the pulse to 10ms
 
 #define DUAL_COIL_TURNOUT(id, en, in1, in2, desc) \
 VIRTUAL_TURNOUT(id, desc) \
@@ -22,6 +26,17 @@ DONE
 
 DUAL_COIL_TURNOUT(1, 47, 39, 41, "Wissel A")
 DUAL_COIL_TURNOUT(2, 49, 43, 45, "Wissel B")
+
+// Define Routes
+ROUTE(RT1,"Station Platform 1")
+    THROW(1)
+    THROW(2)
+    DONE
+
+ROUTE(RT2,"Station Platform 2")
+    CLOSE(1)
+    CLOSE(2)
+    DONE
 
 
 
