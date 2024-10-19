5fc512ae8906c2a861bf0ad92002f6a3cf15f80f
Martin <martin-vl@users.noreply.github.com>
2024-10-19 15:17:45 +0200

    Update mySetup.h

--- mySetup.h
+++ mySetup.h
@@ -9,6 +9,5 @@ SETUP("<S 20 170 pullup>");
 // Create output LEDs for buttons iflag=011 inverted reset at power up
 SETUP("<Z 21 171 3>");
 // Setup time of flight sensor
-SETUP("<S 5000 5000 0>");
-
-//Sensor::create(5000, 5000, 0);
+//SETUP("<S 5000 5000 0>");
+Sensor::create(5000, 5000, 0);
